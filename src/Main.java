c106c73ee041af2164c654b5becb391eb31405f3
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2024-06-17 01:51:47 +0000

    criando o catálogo de filmes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Inside Out 2");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean inclusoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 9.0 + 8.7) / 3;
+        System.out.println(media);
+
+        String sinopse = "Filme Top Gun\nFilme de aventura com galã dos anos 80\nMuito bom!\nAno de Lançamento " + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }
