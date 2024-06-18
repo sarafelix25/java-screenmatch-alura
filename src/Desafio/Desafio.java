0e36a9ed15a25948cd11714206c24c10999dd8f4
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2024-06-18 23:57:03 +0000

    Inicializando os valores da conta

---
+++ src/Desafio/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Sara Felix";
+        String tipoConta = "Corrente";
+        double saldo = 2428.00;
+
+        System.out.println("********************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("********************************");
+    }
+}
