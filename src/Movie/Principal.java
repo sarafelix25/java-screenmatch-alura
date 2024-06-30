d334400b09d7e0d33790e36c10639d6e294f0a77
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2024-06-30 21:39:25 +0000

    controlando acesso e escrita nos dados da app

--- src/Movie/Principal.java
+++ src/Movie/Principal.java
@@ -1,16 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Shrek";
-        meuFilme.anoDeLancamento = 2000;
-        meuFilme.duracaoEmMinutos = 120;
+        meuFilme.setNome("Shrek");
+        meuFilme.setAnoDeLancamento(2002);
+        meuFilme.setDuracaoEmMinutos = 120;
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(9.2);
         meuFilme.avalia(7.4);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.exibeMedia());
+        // meuFilme.somaDasAvaliacoes = 10;
+        // meuFilme.totalDeAvaliacoes = 1;
+        // System.out.println(meuFilme.exibeMedia());
     }
 }
